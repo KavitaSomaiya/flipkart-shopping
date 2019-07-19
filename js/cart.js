f27852014b4d305e6d14464fbe53f374290ea28f
Kavita <49744059+KavitaSomaiya@users.noreply.github.com>
2019-07-19 15:45:54 +0530

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -1,152 +1,32 @@
 ﻿
-function cartHeaderPost(){
-  
-  var cartHeader = [
+function createCartDetails() {
+  let cartData = [
     {
-      homeAndBack: {
-        homeTitle: 'Home',
-        backTitle: 'Back'
-      },
-        imgSrc: './images/favicon.ico'
+      cartImgUrl:'./images/mob- (1).jpeg',
+      quantity: 1,
+      name:'Oppo v15',
+      sellerName:'Omni van retail',
+      price:'250',
+      retailPrice:'1000',
+      discount: 75,
+      offersApplied: 2,
+      deliveryDate: '9 July 2019'
+    }, {
+      cartImgUrl:'./images/umbrella-1.jpeg',
+      quantity: 1,
+      name:'Oppo v15',
+      sellerName:'Omni van retail',
+      price:'250',
+      retailPrice:'1000',
+      discount: 75,
+      offersApplied: 2,
+      deliveryDate: '27 July 2019'
     }
   ],
-  cartHeaderString=''
-  
-  cartHeader.forEach(e => {
-    cartHeaderString =
-    
-      ` 
-      <div class='container-fluid'>
-        <div class='row'>
-          <div class='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 w-100 mw-100'>
-            <nav class='navbar navbar-expand-sm navbar-light w-100 mw-100 justify-content-between' role='navigation'>
-              <div class='col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 p-0 w-100 mw-100'>
-                <div class='navbar-brand' href='#'>
-                  <div class='d-flex align-items-center'>
-                    <a href='#' title="${e.homeAndBack.backTitle}" data-toggle="tooltip" onclick='openIndexPage();'><i class='fas fa-arrow-left ml-2 mr-3' aria-hidden='true'></i></a>
-                    <a href='#' title="${e.homeAndBack.homeTitle}" data-toggle="tooltip" onclick='openIndexPage();'><img class='rounded-circle' alt src='${e.imgSrc}'></a>
-                  </div>
-                </div>              
-              </div>
-              <div class='col-lg-8 col-xl-8 d-lg-block d-xl-block p-0 w-100 mw-100 d-none d-sm-none d-md-none'>
-                <div class="topnav border w-100 mw-100">
-                  <div class="search-container w-100 mw-100 bg-white cart-page-navbar">
-                    <form class='w-100 mw-100' action='#' method='POST'>
-                      <input type="text" placeholder="Search..." name="search">
-                      <button class='bg-white mr-0 pr-0' type="submit">
-                        <i class="fa fa-search"></i>
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <div class='col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 p-0 w-100 mw-100'>
-              <ul class="nav navbar-nav align-items-center justify-content-end float-right">
-                  <div class="dropdown my-0" id='cartPageDropdownId'>
-                      <a class="text-white" href="#" id="navbardrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        User
-                        <i class='fas fa-angle-down text-white fx ml-2 ml-sm-2 ml-md-2 ml-lg-2 ml-xl-2' id='caretId' aria-hidden='true'></i>
-                      </a>
-                      <div class="dropdown-menu dropdown-menu-right py-0" id='dropdownMenu'>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-user-circle mr-3' aria-hidden='true'></i>
-                          My Profile
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-arrows-alt mr-3' aria-hidden='true'></i>
-                          Flipkart Plus Zone
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-gamepad mr-3' aria-hidden='true'></i>
-                          Game Zone
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-folder-plus mr-3' aria-hidden='true'></i>
-                          Orders
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-heart mr-3' aria-hidden='true'></i>
-                          Wishlist
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-file-alt mr-3' aria-hidden='true'></i>
-                          My Chats
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-folder-open mr-3' aria-hidden='true'></i>
-                          Rewards
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-gift mr-3' aria-hidden='true'></i>
-                          Gift Card
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-bell mr-3' aria-hidden='true'></i>
-                          Notification
-                        </a>
-                        <a class="dropdown-item d-flex border-bottom py-3 align-items-center" href="#">
-                          <i class='fas fa-power-off mr-3' aria-hidden='true'></i>
-                          Logout
-                        </a>
-                      </div>
-                  </div>
-                </ul>
-              </div>
-            </nav>            
-          </div>
-          <div class='col-12 col-sm-12 col-md-12 d-block d-sm-block d-md-block d-lg-none d-xl-none p-0'>
-            <div class="topnav border w-100 mw-100">
-              <div class="search-container w-100 mw-100 bg-white cart-page-navbar">
-                <form class='w-100 mw-100' action='#' method='POST'>
-                  <input type="text" placeholder="Search..." name="search">
-                  <button class='bg-white mr-0 pr-0' type="submit">
-                    <i class="fa fa-search"></i>
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      `
-    });
-    
-  document.querySelector('#cartHeaderId').innerHTML = cartHeaderString
-}
-
-
-function createCartDetails() {
-
-let cartData = [
-  {
-    cartImgUrl:'./images/mob- (1).jpeg',
-    quantity: 1,
-    name:'Oppo v15',
-    sellerName:'Omni van retail',
-    price:'250',
-    retailPrice:'1000',
-    discount: 75,
-    offersApplied: 2,
-    deliveryDate: '9 July 2019'
-  }, {
-    cartImgUrl:'./images/umbrella-1.jpeg',
-    quantity: 1,
-    name:'Oppo v15',
-    sellerName:'Omni van retail',
-    price:'250',
-    retailPrice:'1000',
-    discount: 75,
-    offersApplied: 2,
-    deliveryDate: '27 July 2019'
-  }],
   cartDetail =''
-
-//createCartDetails = () => {
- //cartData = parseJSON(window.localStorage.getItem('cartDetail'))
   if (cartData.length > 0) {
     cartData.forEach(item => {
       cartDetail +=
-      
       `
         <div class='d-flex justify-content-between py-4 pl-4 pl-sm-4 pl-md-4 pl-lg-3 pl-xl-3 align-items-top'>
           <div class='mw-100' style="width: 13%;">
@@ -192,23 +72,14 @@ let cartData = [
             </p>                      
           </div>                       
         </div>
-
       `            
-      });
+    });
   } else {
-      cartDetail = `<div>Your cart is empty !!!!!</div>`
+    cartDetail = `<div>Your cart is empty !!!!!</div>`
   }
   document.querySelector('#myCart').innerHTML = cartDetail
   document.querySelector('#totalItemInCart').textContent = `My Cart (${cartData.length})`
-  
 }
-
-//function cartQtyFunc() {
-//  var cartQty = parseInt(document.querySelector('#cartQty').textContent);
-//  cartQty++;
-//  document.querySelector('#cartQty').textContent = cartQty;
-//}
-
 
 document.querySelector('.dropdown').addEventListener('mouseover', mouseoverFunc);
 document.querySelector('.dropdown').addEventListener('mouseout', mouseoverFunc);
@@ -220,7 +91,6 @@ function mouseoverFunc() {
     document.querySelector('#caretId').classList.toggle('fa-angle-up');
     }, 125);
 }
-
 
 function clickDropdown() {
   document.querySelector('.dropdown').removeEventListener('mouseover');
@@ -234,3 +104,8 @@ function clickDropdown() {
   }
 }
 
+//function cartQtyFunc() {
+//  var cartQty = parseInt(document.querySelector('#cartQty').textContent);
+//  cartQty++;
+//  document.querySelector('#cartQty').textContent = cartQty;
+//}
---
+++ js/item-details.js
@@ -0,0 +1,79 @@
+
+function itemDetailBody () {
+  var curItem = JSON.parse(window.localStorage.getItem('curItem'))
+  let mainImgStrng =
+    `
+      <div class="container-fluid">
+        <div class='row no-gutters mt-5 pt-3'>
+          <div class='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
+            <div class='mainImgSectionP'>
+              <p class>On offer</p>
+            </div>
+          </div>
+          <div class='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
+            <div class='mainImgSectionImage'>
+              <img class alt src='${curItem.indexImgUrl}' />
+            </div>
+          </div>
+        <div class='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4' onclick='heartFunc();' type='button'>
+          <div class='rounded-circle shadow-lg mainImgSectionHeart'>
+            <i class='fa fa-heart p-3 heartClassFontSize heartClassGray' id='heartId-${curItem.dataNumber}' aria-hidden='true'></i>
+          </div>
+        </div>
+      </div>
+    `
+  document.querySelector('#mainImgSection').innerHTML = mainImgStrng
+
+  let itemDetailStrng =
+    `
+      <div class='container-fluid'>
+        <div class='row mt-4 pt-2'>
+          <div class='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
+            <div class='d-flex flex-column' id='productDesc'>
+              <div>
+                <h1>
+                  ${curItem.name}
+                  <span><a href='#'>#JustHere</a></span>
+                </h1>
+              </div>
+              <div class='d-flex' id='ratings'>
+                <div class='bg-success h-25 rounded justify-content-center align-items-center px-2 py-1 mr-4 w-auto d-flex'>
+                  <p class='mb-0 text-white mr-1'>4.6</p>
+                  <i class='fa fa-star text-white' aria-hidden='true'></i>
+                </div>
+                <div class='inline-block' id='reviews'>
+                  <p class='mr-4 text-secondary font-weight-bold align-middle'>
+                    69,458 Ratings & 7,914 Reviews
+                    <i class='material-icons align-middle text-secondary'>&#xe315;</i>
+                  </p>
+                </div>
+                <img class alt src='images/fa_8b4b59.png' width='auto' height='24px;' />
+              </div>
+              <p class='text-success font-weight-bold mt-3'>
+                Extra discount
+              </p>
+              <p class='text-black font-weight-bold'>
+                ₹${curItem.price}
+                <span class='mx-4 text-mute' style='text-decoration: line-through;'>
+                  ₹${curItem.retailPrice}
+                </span>
+                <del class='text-success'>
+                  ${curItem.discount}
+                </del>
+              </p>
+              <div class='d-inline-flex align-items-bottom' id='creditCardScheme'>
+                <img class='mr-1' height='18' width='18' alt src='images/scheme-icon.png' /> 
+                <p class=''>Extra 5% off with Axis Bank Buzz Credit Card</p>
+                <i class='material-icons text-secondary' style='font-size: 19.5px;'>&#xe315;</i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  document.querySelector('#itemDetail').innerHTML = itemDetailStrng
+}
+
+function toggleHeartClr(id) {
+  document.getElementById(`heartId-${id}`).classList.toggle('text-danger')
+}
