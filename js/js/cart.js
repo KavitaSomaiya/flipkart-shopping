f47dc00d244d7aa29eb80fc7bfdad83f5b959982
Kavita <49744059+KavitaSomaiya@users.noreply.github.com>
2019-07-23 12:23:10 +0530

    Add files via upload

---
+++ js/js/cart.js
@@ -0,0 +1,112 @@
+﻿
+let curCartItm = JSON.parse(window.localStorage.getItem('curCartItm'))
+
+function createCartData () {
+  let cartDetail =''
+  if (curCartItm.length == 0) {
+    cartDetail = `<div>Your cart is empty !!!!!</div>`
+  } else if (curCartItm.length > 0) {
+    if (curCartItm.length >= 1 ) {
+      curCartItm.forEach(ele => {
+        cartDetail +=
+        `
+          <div class='d-flex justify-content-between py-4 pl-4 pl-sm-4 pl-md-4 pl-lg-3 pl-xl-3 align-items-top'>
+            <div class='mw-100' style="width: 13%;">
+              <div class='d-flex flex-column'>
+                <div class='align-middle cartItemImg text-center p-1 p-sm-1 p-md-1 p-lg-2 p-xl-2'>
+                  <a class='text-center' href='#'>
+                    <img src='${ele.cartImgUrl}' alt${ele.name} />
+                  </a>                          
+                </div>
+                <div class='d-inline-flex justify-content-center align-items-center qtyBtn'>
+                  <button class='rounded-circle shadow-none' type='button'>-</button>
+                  <div class='qtyBtnMiddle'>
+                    <input type='text' value='${ele.quantity}' />
+                  </div>
+                  <button class='rounded-circle shadow-none' type='button'>+</button>
+                </div>         
+              </div>                        
+            </div>
+            <div class='mw-100 cartItemDetails ml-5' style='width: 56%;'>
+              <div class='d-flex flex-column justify-content-start pt-1 pt-sm-1 pt-md-1 pt-lg-2 pt-xl-2'>
+                <div class='itemNameClass'><a class href='#'>${ele.name}</a></div>
+                <p class='sellerName mt-1 mt-sm-1 mt-md-2 mt-lg-3 mt-xl-3'>Seller: ${ele.sellerName}</p>
+                <div class='d-flex mt-1 mt-sm-1 mt-md-1 mt-lg-2 mt-xl-2 justify-content-start align-items-center'>
+                  <span class='itemPrice'>&#x20B9;${ele.price}</span>
+                  <del><span class='itemRealPrice ml-2'>&#x20B9;${ele.retailPrice}</span></del>
+                  <span class='discountPer ml-2'>${ele.discount}</span>
+                  <div class='offerQty ml-2 d-none d-sm-none d-md-block d-lg-block d-xl-block' id='offerQtyId'>
+                    <a>
+                      ${ele.offersApplied}
+                      <i class='' aria-hidden='true'></i>
+                    </a> 
+                  </div>                                            
+                </div> 
+                <div class='d-block justify-content-start mt-1 mt-sm-1 mt-md-2 mt-lg-4 mt-xl-4'>
+                  <button class='px-0 btn btn-link' type='button'>SAVE FOR LATER</button>
+                  <button class='btn btn-link px-0 ml-3' type='button'>REMOVE</button>
+                </div>
+              </div>
+            </div>
+            <div class='mw-100 pt-1 pt-sm-1 pt-md-1 pt-lg-2 pt-xl-2 deliveryDetails' style="width: 31%;">
+              <p>
+                Delivery by, ${ele.deliveryDate}
+              </p>                      
+            </div>                       
+          </div>
+        `
+      })} else {
+        cartDetail +=
+        `
+          <div class='d-flex justify-content-between py-4 pl-4 pl-sm-4 pl-md-4 pl-lg-3 pl-xl-3 align-items-top'>
+            <div class='mw-100' style="width: 13%;">
+              <div class='d-flex flex-column'>
+                <div class='align-middle cartItemImg text-center p-1 p-sm-1 p-md-1 p-lg-2 p-xl-2'>
+                  <a class='text-center' href='#'>
+                    <img src='${ele.cartImgUrl}' alt${ele.name} />
+                  </a>                          
+                </div>
+                <div class='d-inline-flex justify-content-center align-items-center qtyBtn'>
+                  <button class='rounded-circle shadow-none' type='button'>-</button>
+                  <div class='qtyBtnMiddle'>
+                    <input type='text' value='${ele.quantity}' />
+                  </div>
+                  <button class='rounded-circle shadow-none' type='button'>+</button>
+                </div>         
+              </div>                        
+            </div>
+            <div class='mw-100 cartItemDetails ml-5' style='width: 56%;'>
+              <div class='d-flex flex-column justify-content-start pt-1 pt-sm-1 pt-md-1 pt-lg-2 pt-xl-2'>
+                <div class='itemNameClass'><a class href='#'>${ele.name}</a></div>
+                <p class='sellerName mt-1 mt-sm-1 mt-md-2 mt-lg-3 mt-xl-3'>Seller: ${ele.sellerName}</p>
+                <div class='d-flex mt-1 mt-sm-1 mt-md-1 mt-lg-2 mt-xl-2 justify-content-start align-items-center'>
+                  <span class='itemPrice'>&#x20B9;${ele.price}</span>
+                  <del><span class='itemRealPrice ml-2'>&#x20B9;${ele.retailPrice}</span></del>
+                  <span class='discountPer ml-2'>${ele.discount}</span>
+                  <div class='offerQty ml-2 d-none d-sm-none d-md-block d-lg-block d-xl-block' id='offerQtyId'>
+                    <a>
+                      ${ele.offersApplied}
+                      <i class='' aria-hidden='true'></i>
+                    </a> 
+                  </div>                                            
+                </div> 
+                <div class='d-block justify-content-start mt-1 mt-sm-1 mt-md-2 mt-lg-4 mt-xl-4'>
+                  <button class='px-0 btn btn-link' type='button'>SAVE FOR LATER</button>
+                  <button class='btn btn-link px-0 ml-3' type='button'>REMOVE</button>
+                </div>
+              </div>
+            </div>
+            <div class='mw-100 pt-1 pt-sm-1 pt-md-1 pt-lg-2 pt-xl-2 deliveryDetails' style="width: 31%;">
+              <p>
+                Delivery by, ${ele.deliveryDate}
+              </p>                      
+            </div>                       
+          </div>
+        `
+      }
+    }
+  document.querySelector('#myCart').innerHTML = cartDetail
+  document.querySelector('#totalItemInCart').textContent = `My Cart (${curCartItm.length})`
+}
+
+
---
+++ js/js/index.js
@@ -0,0 +1,230 @@
+
+let indexData = [
+  {
+    indexImgUrl:'./images/mob-1.jpeg',
+    name:'Vivo V15 Pro (Topaz Blue, 128 GB)  (6 GB RAM)',
+    price:'26,990',
+    retailPrice:'32,990',
+    discount: 18,
+    dataNumber: 1
+  }, {
+    indexImgUrl:'./images/mob-2.jpeg',
+    name:'Honor 9N (Robin Egg Blue, 64 GB)  (4 GB RAM)',
+    price:'14,500',
+    retailPrice:'15833',
+    discount: 8,
+    dataNumber: 2
+  }, {
+    indexImgUrl:'./images/mob-3.jpeg',
+    name:'Redmi Note 6 Pro (Black, 64 GB)  (6 GB RAM)',
+    price:'13999',
+    retailPrice:'17999',
+    discount: 22,
+    dataNumber: 3
+  }, {
+    indexImgUrl:'./images/mob-4.jpeg',
+    name:'Redmi Note 6 Pro (Red, 64 GB)  (4 GB RAM)',
+    price:'11,999',
+    retailPrice:'15,999',
+    discount: 25,
+    dataNumber: 4
+  }, {
+    indexImgUrl:'./images/mob-5.jpeg',
+    name:'Redmi Note 6 Pro (Blue, 64 GB)  (6 GB RAM)',
+    price:'13999',
+    retailPrice:'17999',
+    discount: 22,
+    dataNumber: 5
+  }, {
+    indexImgUrl:'./images/mob-6.jpeg',
+    name:'Redmi Note 7S (Ruby Red, 64 GB)  (4 GB RAM)',
+    price:'13999',
+    retailPrice:'15999',
+    discount: 12,
+    dataNumber: 6
+  }, {
+    indexImgUrl:'./images/mob-7.jpeg',
+    name:'Redmi Note 7 Pro (Nebula Red, 128 GB)  (6 GB RAM)',
+    price:'16,999',
+    retailPrice:'17,999',
+    discount: 5,
+    dataNumber: 7
+  }, {
+    indexImgUrl:'./images/mob-8.jpeg',
+    name:'OPPO F11 Pro (Thunder Black, 64 GB)  (6 GB RAM)',
+    price:'20,990',
+    retailPrice:'28,990',
+    discount: 27,
+    dataNumber: 8
+  }, {
+    indexImgUrl:'./images/mob-9.jpeg',
+    name:'OPPO K1 (Piano Black, 64 GB)  (4 GB RAM)',
+    price:'12,990',
+    retailPrice:'18,990',
+    discount: 31,
+    dataNumber: 9
+  }, {
+    indexImgUrl:'./images/mob-10.jpeg',
+    name:'Redmi Note 7 Pro (Space Black, 128 GB)  (6 GB RAM)',
+    price:'16,999',
+    retailPrice:'17,999',
+    discount: 5,
+    dataNumber: 10
+  }, {
+    indexImgUrl:'./images/umbrella-1.jpeg',
+    name:'Dragon Kids Head Umbrella Umbrella  (Multicolor)',
+    price:'139',
+    retailPrice:'499',
+    discount: 72,
+    dataNumber: 11
+  }, {
+    indexImgUrl:'./images/umbrella-2.jpeg',
+    name:'eweft Rainbow Sun and Rain Protection And Windproof Umbrella  (Multicolor)',
+    price:'440',
+    retailPrice:'700',
+    discount: 37,
+    dataNumber: 12
+  }, {
+    indexImgUrl:'./images/umbrella-3.jpeg',
+    name:'FabSeasons Big Polka Dots all Seasons Umbrella  (Maroon)',
+    price:'555',
+    retailPrice:'999',
+    discount: 44,
+    dataNumber: 13
+  }, {
+    indexImgUrl:'./images/chappal-1.jpeg',
+    name:'SOLAY THONG Flip Flops',
+    price:'638',
+    retailPrice:'1595',
+    discount: 60,
+    dataNumber: 14
+  }, {
+    indexImgUrl:'./images/shoes-1.jpeg',
+    name:'Provogue Sneakers For Men  (Grey)',
+    price:'799',
+    retailPrice:'1999',
+    discount: 60,
+    dataNumber: 15
+  }, {
+    indexImgUrl:'./images/shoes-2.jpeg',
+    name:'Lee Cooper Slip On For Men  (Black)',
+    price:'1,484',
+    retailPrice:'2699',
+    discount: 45,
+    dataNumber: 16
+  }, {
+    indexImgUrl:'./images/jeans-1.jpeg',
+    name:'Jealous 21 Skinny Women Blue Jeans',
+    price:'509',
+    retailPrice:'1644',
+    discount: 68,
+    dataNumber: 17
+  }, {
+    indexImgUrl:'./images/jeans-2.jpeg',
+    name:'Jealous 21 Slim Women Light Blue Jeans',
+    price:'509',
+    retailPrice:'1699',
+    discount: 70,
+    dataNumber: 18
+  }, {
+    indexImgUrl:'./images/chappal-2.jpeg',
+    name:'CHROMA THONG 5 Slippers',
+    price:'747',
+    retailPrice:'1065',
+    discount: 50,
+    dataNumber: 19
+  }, {
+    indexImgUrl:'./images/saree-1.jpeg',
+    name:'HNC Retail Floral Print Fashion Pure Georgette, Chiffon Saree  (Green)',
+    price:'499',
+    retailPrice:'2498',
+    discount: 80,
+    dataNumber: 20
+  }
+],
+indexDetails =''
+createIndexImg = () => {
+  indexData.forEach(item => {
+    const id = item.dataNumber
+    indexDetails +=      
+      `
+        <li class='heartLiClass'>
+          <section class='sortFilterSection m-0 p-0 cust-ouline-0 w-100 mw-100 backHtmlImgList'>
+            <div class="container-fluid">
+              <div class='row no-gutters' id='backHtmlOnOffer'>
+                <div class='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
+                  <div class='mainImgSectionP'>
+                    <p class='px-1'>On offer</p>
+                  </div>
+                </div>
+              </div>
+              <div class='row no-gutters my-4 my-sm-4 my-md-4 my-lg-4 my-xl-4' id='backHtmlImg'>
+                <div class='col-2 col-sm-2 col-md-3 col-lg-2 col-xl-2'>
+                  <div>
+                    <div class='backHtmlImgClass' onclick="goToItemDetail('${id}');">
+                      <img alt src='${item.indexImgUrl}' /> 
+                    </div>
+                  </div>
+                </div>
+                <div class='col-8 col-sm-8 col-md-8 col-lg-9 col-xl-9 px-1'>
+                  <div class='d-flex flex-column' id='productDesc2'>
+                    <div>
+                      <h1 class onclick="goToItemDetail('${id}');">
+                        ${item.name}
+                        <span><a href='#'>#JustHere</a></span>
+                      </h1>
+                      <p class='text-black font-weight-bold mb-0'>
+                        ₹${item.price}
+                        <span class='mx-4 text-mute' style='text-decoration: line-through;'>₹${item.retailPrice}</span>
+                        <del class='text-success'>${item.discount}% off</del>
+                      </p>
+                      <div class='d-inline-flex align-items-bottom'>
+                        <p class='mt-2 pt-0 font-weight-bold'>
+                          Exchange available
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class='col-2 col-sm-2 col-md-1 col-lg-1 col-xl-1 px-1 heartClassSection bg-white'  type='button'>
+                  <div class='row no-gutters justify-content-center align-items-center'>
+                    <div class='mainImgSectionHeart w-50 text-center'>
+                      <i onclick='toggleHeartClr(event);' class='heartClassFontSize heartClassGray heartClass fa fa-heart p-2 p-sm-2 p-md-2 py-lg-3 px-lg-2 py-xl-4 px-xl-1' aria-hidden='true'></i>
+                    </div> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </li>
+      `
+    });
+  document.querySelector('#indexImgId').innerHTML = indexDetails
+}
+
+function goToItemDetail (it) {
+  let itm = parseInt(it)
+  let curItem = indexData.filter(i => i.dataNumber === itm)
+  console.log(curItem)
+  window.localStorage.setItem('curItem', JSON.stringify(curItem[0]))
+  window.location.href = './item-details.html'
+}
+
+function goToIndexPage () {
+  window.open('./index.html','_self','scrollbars=yes,menubar=yes');
+  window.focus()
+}
+
+function goToWishList () {
+  window.open('./wish-list.html','_self','scrollbars=yes,menubar=yes');
+  window.focus()
+}
+
+function srch () {
+  document.querySelector('#searchId').classList.toggle('d-block')
+}
+
+function toggleHeartClr (e) {
+  e.target.classList.toggle('text-danger')
+}
+
